4721cdd8df93c99981720f315cc33ce1917485bc
Daniel Bolgheroni <dbolgheroni@protonmail.com>
2023-07-31 09:30:59 -0300

    Implement the /stock command
    It works with some tests made but still need some work on i1-chat to configure
    the exchanges and queues properly.
    
    The plan for now is to use a direct exchange with the consumers (chat clients)
    creating the queues and binding this queue to the routing key with the name of
    the room (room1, room2, etc. for simplicity).
    
    Other patterns are being considered and using other patterns such as the topic
    or even changing a direct exchange for a header exchange.

--- i1-bot/i1-bot.cpp
+++ i1-bot/i1-bot.cpp
@@ -1,29 +1,117 @@
+#include <chrono>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
+#include <cctype>
 
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 
-int main() {
+#include <SimpleAmqpClient/SimpleAmqpClient.h>
+
+std::string run_cmd_stock(const std::string&);
+std::string parse_stock_price_from_csv(const std::string&);
+
+// Check if the body of the message contains a command and
+// reach Stooq if it has.
+std::string parse_cmd(const AmqpClient::BasicMessage::ptr_t message) {
+    auto cmd = false;
+
+    // Find the /stock command in the message body.
+    auto body = message->Body();
+
+    auto say = body;
+    if (body.rfind("/stock=", 0) != std::string::npos) {
+        auto stock = body.substr(body.find("=") + 1);
+        say = run_cmd_stock(stock);
+    }
+
+    return say;
+}
+
+std::string parse_stock_price_from_csv(const std::string& csv) {
+    // Stooq returns 2 rows even for a single stock request.
+    // The first is just a description of the columns of the CSV.
+    // Skip the first row and get the second one.
+    auto row = csv.substr(csv.find("\n") + 1);
+    std::cout << "debug: " << row;
+
+    // Parse value for row.
+    auto end = row.rfind(',');
+    auto beg = row.rfind(',', end - 1) + 1;
+    auto quote = row.substr(beg, end - beg);
+
+    return quote;
+}
+
+std::string run_cmd_stock(const std::string& stock) {
+    //std::cout << "cmd: " << stock << std::endl;
+
+    std::string csvResponse;
+
+    // Make a request to Stooq using curlpp.
     try {
         curlpp::Easy request;
 
-        request.setOpt(new curlpp::options::Url(
-            "https://stooq.com/q/l/?s=aapl.us&f=sd2t2ohlcv&h&e=csv"));
+        std::string url = "https://stooq.com/q/l/?s=" + stock +
+            "&f=sd2t2ohlcv&h&e=csv";
+        request.setOpt(new curlpp::options::Url(url));
 
         std::stringstream responseStream;
         request.setOpt(new curlpp::options::WriteStream(&responseStream));
 
         request.perform();
-
-        std::string csvResponse = responseStream.str();
-
-        std::cout << csvResponse;
-
+        csvResponse = responseStream.str();
     } catch (curlpp::RuntimeError& e) {
         std::cerr << "error: " << e.what() << std::endl;
     } catch (curlpp::LogicError& e) {
+        std::cerr << "error: " << e.what() << std::endl;
+    }
+
+    auto quote = parse_stock_price_from_csv(csvResponse);
+
+    std::string say;
+    if (quote == "N/D") {
+        say = "There is no such stock.";
+    } else {
+        say = stock + " quote is $" + quote + " per share.";
+    }
+
+    return say;
+}
+
+int main() {
+    std::string exchangeName = "i1-chat";
+    std::string routingKey = "i1-chat";
+
+    try {
+        AmqpClient::Channel::ptr_t channel =
+            AmqpClient::Channel::Create("localhost");
+
+        std::cout << "i1-bot debug messages" << std::endl;
+
+        std::string queueName {"room1"};
+
+        std::string tag = channel->BasicConsume(queueName, "");
+
+        while(1) {
+            AmqpClient::Envelope::ptr_t envelope =
+                channel->BasicConsumeMessage(tag);
+
+            if (envelope) {
+                auto message = envelope->Message();
+                auto say = parse_cmd(message);
+
+                std::cout << "publishing: " << say << std::endl;
+
+                AmqpClient::BasicMessage::ptr_t response =
+                    AmqpClient::BasicMessage::Create(say);
+
+                channel->BasicPublish(exchangeName, routingKey, response);
+            }
+        }
+    } catch (const std::exception& e) {
         std::cerr << "error: " << e.what() << std::endl;
     }
 
