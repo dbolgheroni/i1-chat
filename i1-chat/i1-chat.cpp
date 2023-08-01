5c1bc3263beaf0f11b98444f6b7e492bf9188f5f
Daniel Bolgheroni <dbolgheroni@protonmail.com>
2023-07-31 22:55:18 -0300

    Create the exchange and queues on the backend
    Also log what is happening on stdout. The idea is to have the backend creating
    all the infrastructure on the broker so the clients and the bots can operate
    upon.
    
    Things like attributes of the exchanges and queues are made explicit although
    somewhat more verbose. This makes it easier to match what's on the bot also.

--- i1-chat/i1-chat.cpp
+++ i1-chat/i1-chat.cpp
@@ -3,27 +3,61 @@
 #include <string>
 
 int main() {
+    std::string exchangeName = "i1-chat";
+    std::string room1Name = "room1";
+    std::string room2Name = "room2";
+
+    std::cout << "info: starting i1-chat" << std::endl;
+
+    AmqpClient::Channel::ptr_t channel;
+
     try {
-        AmqpClient::Channel::ptr_t channel =
-            AmqpClient::Channel::Create("localhost");
+        channel = AmqpClient::Channel::Create("localhost");
 
-        std::string exchangeName = "i1-chat";
+
+        // Create the i1-chat exchange. That will be the main exchange for the
+        // backend and for all clients connecting to it.
         std::string exchangeType = "direct";
-        bool durable = true;
-        bool autoDelete = true;
+        bool exchangePassive = false;
+        bool exchangeDurable = false;
+        bool exchangeAutoDelete = false;
 
-        channel->DeclareExchange(exchangeName, exchangeType, durable, autoDelete);
+        std::cout << "info: creating " << exchangeName << " exchange" << std::endl;
+        channel->DeclareExchange(exchangeName, exchangeType,
+            exchangePassive, exchangeDurable, exchangeAutoDelete);
 
-        std::string routingKey = "room1";
-        std::string messageBody = "Hello room1 from i1-chat already!";
+        // Create the queues. Each queue represents a room. For now, the number
+        // of rooms are fixed and the users cannot create new rooms.
+        auto queuePassive = false;
+        auto queueDurable = false;
+        auto queueExclusive = false;
+        auto queueAutoDelete = false;
 
-        AmqpClient::BasicMessage::ptr_t message =
-            AmqpClient::BasicMessage::Create(messageBody);
+        std::cout << "info: creating " << room1Name << " queue" << std::endl;
+        channel->DeclareQueue(room1Name,
+            queuePassive, queueDurable, queueExclusive, queueAutoDelete);
 
-        channel->BasicPublish(exchangeName, routingKey, message);
+        std::cout << "info: creating " << room2Name << " queue" << std::endl;
+        channel->DeclareQueue(room2Name,
+            queuePassive, queueDurable, queueExclusive, queueAutoDelete);
+
+        // queue, consumer_tag, no_local, no_ack, exclusive
+        std::string tag = channel->BasicConsume("room1", "");
+
+        // Since BasicConsumeMessage is blocking, wait on it until a new
+        // message arrives.
+        for(;;) {
+            AmqpClient::Envelope::ptr_t envelope =
+                channel->BasicConsumeMessage(tag);
+
+            if (envelope) {
+                std::cout << "debug: received message" << std::endl;
+            }
+        }
     } catch (const std::exception& e) {
         std::cerr << "error: " << e.what() << std::endl;
     }
+
 
     return 0;
 }
